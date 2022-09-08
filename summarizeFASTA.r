ff2ecde34489f203a7a0aa098d692811f2506af3
Yasoo Morimoto <yasoo4253@gmail.com>
2022-09-08 11:55:36 +0200

    New features to the fasta tools
    Now it can search sequences

--- summarizeFASTA.r
+++ summarizeFASTA.r
@@ -2,6 +2,8 @@
 
 # Print number of sequences --summary
 # Print names of sequences --names
+# search a sequence --match
+# TODO: also search complement and reverse complement
 
 # Written by Yasoo Morimoto
 
@@ -9,21 +11,57 @@ library("seqinr")
 library("optparse")
 
 optionList <- list(make_option(c("-v", "--verbose"), action = "store_true",
-																default = TRUE, help = "Print extra output [default]"),
+																default = FALSE, help = "Print extra output [default]"),
 make_option(c("-s", "--summary"), action = "store_true", help = "", default = FALSE),
-make_option(c("-n", "--names"), action = "store_true", default = FALSE))
+make_option(c("-n", "--names"), action = "store_true", default = FALSE),
+make_option(c("-m", "--match"), action = "store"))
 
 opt <- parse_args(OptionParser(option_list = optionList,
 															 usage = "usage: %prog [sequences.fasta]"),
 									positional_arguments = 1)
 
+aaORdna <- function(lst) {
+	for (s in lst) {
+		if (sum(summary(s)$composition) == length(s)) {
+			if (opt$options$verbose) {
+				print("its DNA")
+			}
+		} else {
+			if (opt$options$verbose) {
+				print("its not DNA, setting to AA")
+			}
+			attr(s, "class") <- "SeqFastaAA"
+		}
+	}
+	lst
+}
+
+searchSequence <- function(s, searchSeq) {
+	sq <- paste(s, collapse = "")
+	startpos <- unlist(gregexpr(searchSeq, sq))
+	if (startpos > 0) {
+	cat(paste("Found query in the sequence",
+				getName(s),
+				"at position",
+				startpos,
+				"to",
+				startpos + nchar(searchSeq)))
+	}
+
+}
+
+
+
 seqs <- read.fasta(file = opt$args[1])
+seqs <- aaORdna(seqs)
 
 
 if (opt$options$summary) {
 	cat("Number of sequences:", length(seqs), "\n")
 } else if (opt$options$names) {
+	cat(getName(seqs), sep = "\n")
+} else if (length(opt$options$match) != 0) {
 	for (s in seqs) {
-		cat(attr(s, "name"), "\n")
+		searchSequence(s, opt$options$match)
 	}
 }
