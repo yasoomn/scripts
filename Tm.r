28a5a1f88ac3fb4ca7306fcc31eb04b33300b339
yasoo <yasoo4253@gmail.com>
2023-05-03 15:37:56 +0200

    Added Tm.r script
    This is a script for calcuating the primer Tm according to the formula
    given by Takara Bio

---
+++ Tm.r
@@ -0,0 +1,19 @@
+#!/bin/Rscript
+
+args <- commandArgs(trailingOnly=T)
+# test if there is at least one argument: if not, return an error
+if (length(args)==0) {
+  stop("At least one argument must be supplied", call.=FALSE)
+} else {
+  oligo = toupper(args[1])
+}
+
+#oligo = "AAACTGGACTGGGC"
+
+nAT <- length(grep("A|T", strsplit(oligo, "")[[1]], value = F))
+nCG <- length(grep("C|G", strsplit(oligo, "")[[1]], value = F))
+
+
+Tm <- (nAT * 2) + (nCG * 4) - 5
+
+print(paste(Tm, " °C"))
