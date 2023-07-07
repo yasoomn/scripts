3ea90f9dbfcc3a64ebc2d9357f216fcdb306f2b4
yasoo <yasoo4253@gmail.com>
2023-07-07 17:34:50 +0200

    rewrote the script to accomodate kinetic measurement
    The first itearation of the script was made to deal with files produced
    in point measurements. This version deals with multiple measurements
    over a long period of time. Additionally, it also takes a "label" file,
    which contains labels for each well. The labels are added to the
    respective well entries.
    
    Finally, the optparse package was integrated for more elegant control.

--- tecan.r
+++ tecan.r
@@ -2,62 +2,89 @@
 
 packages <- c("tidyr", "dplyr")
 
-install.packages(setdiff(packages, rownames(installed.packages())))  
+install.packages(setdiff(packages, rownames(installed.packages())))
 
 suppressPackageStartupMessages({
 
 library(tidyr)
 library(dplyr)
+library(optparse)
 })
 
-args <- commandArgs(trailingOnly=T)
-# test if there is at least one argument: if not, return an error
-if (length(args)==0) {
-  stop("At least one argument must be supplied (input file).csv", call.=FALSE)
-} else {
-  tecanOutput <- read.csv(file = args[1])
-	labelPlate <- args[2] 
+
+optionList <- list(make_option(c("-v", "--verbose"), action = "store_true",
+																default = FALSE, help = "Print extra output [default]"),
+make_option(c("-o", "--OD"), action = "store", help = "File containing the table with optical density measurements"),
+make_option(c("-m", "--measurement"), action = "store"),
+make_option(c("-l", "--labels"), action = "store", help = "")
+)
+
+opt <- parse_args(OptionParser(option_list = optionList,
+															 usage = "usage: %prog [sequences.fasta]")
+)
+
+#opt <- list()
+#opt$OD <- "./andersonOD.csv"
+#opt$measurement <- "./andersonLux.csv"
+#opt$labels <- "./andersonPlate.csv"
+
+
+# parse OD values
+
+#data <- read.csv("./andersonOD.csv", row.names = 1)
+
+
+elongate_data <- function(csv_path) {
+	# load the file
+	data <- read.csv(csv_path, row.names = 1)
+# Separate rows with temperature and time
+	data.info <- data[1:2, ] %>% 
+		t() %>% 
+		as.data.frame()
+	# Add column with cycle number
+	data.info$cycle <- c(1:nrow(data.info))
+	# rename columns
+	names(data.info) <- c("time", "temp", "cycle")
+
+	# Separate rows with measurements
+	data.values <- data %>% slice_tail(n = nrow(data) - 2) %>% t() %>% as.data.frame()
+	data.values$cycle <- c(1:nrow(data.values))
+	data.values.long <- data.values %>% pivot_longer(cols = 1:96)
+	
+	# Combine both
+	data.long <- full_join(data.info, data.values.long, by = "cycle") 
+	data.long
 }
 
-firstLine <- grep("Zyklus", tecanOutput$Programm..Tecan.i.control)
 
-tecanOutput.trimmed <- read.csv(args[1], skip = firstLine, nrows = 100)
-
-# discard columns with no information and rows with NAs
-tecanOutput.trimmed <- na.omit(tecanOutput.trimmed[,colSums(is.na(tecanOutput.trimmed))<nrow(tecanOutput.trimmed)])
-
-tecanOutput.cycles <- tecanOutput.trimmed %>% 
-	filter(row_number() > 2) %>% 
-	pivot_longer(cols = c(2:4), names_to = "cycle", names_prefix = "X")
+long.output <- elongate_data(opt$measurement)
+names(long.output) <- c("time", "temp", "cycle", "well", "measurement")
 
 
-tecanOutput.info <- read.csv(args[1], skip = firstLine - 1) %>% 
-	filter(row_number() < 4) %>% 
-	t()
-
-colnames(tecanOutput.info) <- c("cycle", "time", "temp")
-colnames(tecanOutput.cycles) <- c("well", "cycle", "value")
-
-tecanOutput.info <- as.data.frame(tecanOutput.info) %>% 
-	filter(row_number() > 1) %>% 
-	drop_na()
-
-tecanOutput.info$cycle <- as.double(tecanOutput.info$cycle)
-tecanOutput.cycles$cycle <- as.double(tecanOutput.cycles$cycle)
+# Create the X and Y positions of the wells  as separate columns
+	long.output <- long.output %>% 
+		rowwise() %>%
+		mutate(well_x = substr(well, 2, 3), well_y = substr(well, 1, 1))
 
 
-## handle labels
 
-if (!is.na(args[2])) {
-	## read labels table
-	plate = read.csv(args[2], header = T, row.names = 1)
+
+# add the OD measurement
+
+if (!is.null(opt$OD)) {
+	long.OD <- elongate_data(opt$OD)
+	names(long.OD) <- c("time", "temp", "cycle", "well", "OD")
+	long.output <- full_join(long.output, long.OD %>% select(well, cycle, OD), by = c("cycle", "well" ))
+	
+}
+
+if (!is.null(opt$labels)) {
+	plate = read.csv(opt$labels, header = T, row.names = 1, colClasses = "character")
 	# create a data.frame with the correct dimensions and a well column and add the labels
 	plate_long <- data.frame(well = paste(rep(LETTERS[1:8], each = 12), 1:12, sep = ""), 
 	label = pivot_longer(plate, values_to = "label", cols = 1:12)$label)
-	output <- inner_join(tecanOutput.info, tecanOutput.cycles) %>%inner_join(plate_long)
-} else {
-	## If no table with well labels is provided, just output the contents in long format
-	output <- inner_join(tecanOutput.info, tecanOutput.cycles)
+	long.output <- inner_join(long.output, plate_long, by = "well")
+
 }
 
-write.csv(output, file = "", row.names = F)
+write.csv(long.output, file = "", row.names = F)
